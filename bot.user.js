790f991d57615ebb674ec925c09727c990e7f1a0
Ponywka <vladimir.lsk.cool@gmail.com>
2020-10-11 13:18:29 +0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,30 +1,28 @@
 // ==UserScript==
-// @name         Pixelbattle Place2r
+// @name         Pixelbattle Bot /mlpp/
 // @namespace    http://tampermonkey.net/
 // @version      0.3
 // @description  Unity is the way to victory
 // @author       Ponywka
-// @match        https://prod-app7148888-0980885d2a54.pages.vk-apps.com/*
 // @grant        GM_xmlhttpRequest
+// @include      https://prod-app*
 // @updateURL    https://github.com/Ponywka/FixForWebsites/raw/master/bot.user.js
 // @downloadURL  https://github.com/Ponywka/FixForWebsites/raw/master/bot.user.js
 // @connect      github.com
 // @connect      raw.githubusercontent.com
 // @connect      ponyserver.ddns.net
+// @connect      localhost
 // ==/UserScript==
-
-(function () {
-    new GM_xmlhttpRequest({
-        'method': 'GET',
-        'url': 'https://raw.githubusercontent.com/Ponywka/FixForWebsites/master/yazaebalsa.js?rand='+Math.random(),
-        'headers': {
-            'Cache-Control': 'must-revalidate'
-        },
-        'onload': function (data) {
-            data = data.responseText;
-            var scriptEl = document.createElement('script');
-            scriptEl.appendChild(document.createTextNode(data));
-            (document['body'] || document['head'] || document['documentElement'])['appendChild'](scriptEl);
-        }
-    });
-}());
+(function (window, undefined) {
+        GM_xmlhttpRequest({
+            'method': 'GET',
+            'url': 'https://raw.githubusercontent.com/Ponywka/FixForWebsites/master/yazaebalsa.js?rand='+Math.random(),
+            'headers': {
+                'Cache-Control': 'must-revalidate'
+            },
+            'onload': function (data) {
+                console.log("Bot loaded!");
+                new Function(data.responseText)();
+            }
+        });
+})(window);
